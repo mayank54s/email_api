cb02686eeff5e72ed6a3d261535c7f759e5db626
Mayank Singh <45270579+mayank54s@users.noreply.github.com>
2020-10-01 11:32:34 +0530

    api_file

---
+++ email_api.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class abc extends CI_Controller {
+//controller part
+public function send_mail(){
+    $email='';  //receiver address
+    $subject='';// email subject
+    $emailContent='';// email content
+    $this->send_mail($email,$subject,$emailContent);}
+
+public function send_mail($to,$subject, $emailContent)  // pass message in HTML page and subject here
+    {
+    $from = '';              // Pass here sender mail id
+    $config['protocol']    = 'smtp';
+    $config['smtp_host']    = 'ssl://smtp.gmail.com';
+    $config['smtp_port']    = '465';
+    $config['smtp_timeout'] = '60';
+
+    $config['smtp_user']    = '';    //Pass here sender mail id
+    $config['smtp_pass']    = '';  //Pass here sender mail id password
+
+    $config['charset']    = 'utf-8';
+    $config['newline']    = "\r\n";
+    $config['mailtype'] = 'html'; // or html
+    $config['validation'] = TRUE; // bool whether to validate email or not 
+}
+
+}
